5879a220d60638b52bde8907c34e846f3edeccf6
Kashish <kashish042001@gmail.com>
2022-11-19 19:54:07 +0530

    Header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./header.css";
+import "./Header.css";
 import { Avatar } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import {useDataLayerValue} from "./DataLayer";

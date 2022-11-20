00bca28ec131d6b22e14849b7bcc7a195371ff7f
Khushal-ag <starkhush5@gmail.com>
2022-11-20 10:55:11 +0530

    fix imports

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,15 @@
+import React from "react";
 import "./Login.css";
-
 import { loginUrl } from "../constants/heavens";
 
 const Login = () => {
 	return (
-		<div className="login">
+		<div className="Login">
 			<img
-				src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-				alt="Spotify Logo"
+				src="/assets/image/HeavenMusic Logo.png"
+				alt="HeavensMusic Logo"
 			/>
-			<a href={loginUrl}>Login With Spotify</a>
+			<a href={loginUrl}>Click Here</a>
 		</div>
 	);
 };
